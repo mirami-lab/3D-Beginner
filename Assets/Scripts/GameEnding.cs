5903301c8a98695d587655e3a524ef0602b30230
mirami <maya.hunter2@loop.colum.edu>
2019-11-20 10:14:40 -0600

    project1

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -10,11 +10,14 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource caughtAudio;
 
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
     float m_Timer;
+    bool m_HasAudioPlayed;
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
@@ -34,16 +37,21 @@ public class GameEnding : MonoBehaviour
     {
         if(m_IsPlayerAtExit)
         {
-            EndLevel (exitBackgroundImageCanvasGroup, false);
+            EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
         }
         else if (m_IsPlayerCaught)
         {
-            EndLevel (caughtBackgroundImageCanvasGroup, true);
+            EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     } 
 
-    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)                      
     {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
         if(m_Timer > fadeDuration + displayImageDuration)
